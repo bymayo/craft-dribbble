2226a115ffeb8d64b9de92e58a60ff0a0e1539dc
Jason Mayo <jason@bymayo.co.uk>
2018-02-26 16:07:22 +0000

    Updated versions

--- dribbble/DribbblePlugin.php
+++ dribbble/DribbblePlugin.php
@@ -43,12 +43,12 @@ class DribbblePlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     public function getSchemaVersion()
     {
-        return '1.0.1';
+        return '1.0.2';
     }
 
     public function getDeveloper()
